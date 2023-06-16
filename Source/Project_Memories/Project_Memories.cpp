ed61f5a2730cb6a0d4be925ce4b90b8c8fd5bda4
Craitter <jannis1.9.9.8@web.de>
2023-06-16 20:11:54 +0200

    Initial commit
    test if you have to build

---
+++ Source/Project_Memories/Project_Memories.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Project_Memories.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Project_Memories, "Project_Memories" );
---
+++ Source/Project_Memories/Project_MemoriesGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Project_MemoriesGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_MEMORIES_API AProject_MemoriesGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
